52394bb8f01c56129b1ebd99262e30d5db3a08e0
Alex7498-SV <00068817>
2019-03-23 00:06:56 -0600

    avances finales

--- app/src/main/java/com/alex7498/labo3/newActivity.java
+++ app/src/main/java/com/alex7498/labo3/newActivity.java
@@ -3,6 +3,7 @@ package com.alex7498.labo3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.alex7498.labo3.Utils.AppConstants;
@@ -10,6 +11,7 @@ import com.alex7498.labo3.Utils.AppConstants;
 public class newActivity extends AppCompatActivity {
 
     private TextView mUser, mPass, mEmail, mGenero;
+    private Button mShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,7 @@ public class newActivity extends AppCompatActivity {
         mPass = findViewById(R.id.passText);
         mEmail = findViewById(R.id.emailText);
         mGenero = findViewById(R.id.generoText);
-
+        mShare = findViewById(R.id.share);
         Intent mIntent = this.getIntent();
 
         if(mIntent != null){
@@ -29,6 +31,14 @@ public class newActivity extends AppCompatActivity {
             mPass.setText(text[1]);
             mEmail.setText(text[2]);
             mGenero.setText(text[3]);
+
+            mShare.setOnClickListener(e->{
+                Intent mIntent2 = new Intent();
+                mIntent2.setAction(Intent.ACTION_SEND);
+                mIntent2.setType("text/plain");
+                mIntent2.putExtra(Intent.EXTRA_TEXT, text[0]+text[1]+text[2]+text[3]);
+                startActivity(mIntent2);
+            });
         }
     }
 }
